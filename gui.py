44d63dc5b1664e2747d8411d5fa7d71af88dc935
EldarKlyuev <e.s.klyuev@gmail.com>
2022-06-26 19:37:19 +0300

    Add up, down and clear methods

--- gui.py
+++ gui.py
@@ -14,13 +14,15 @@ class DlgMain(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.setupUi(self)
         self.pushButton.clicked.connect(self.evt_opn_btn_click)
         self.pushButton_2.clicked.connect(self.evt_sv_btn_click)
+        self.downButton.clicked.connect(self.evt_down_btn_click)
+        self.upButton.clicked.connect(self.evt_up_btn_click)
+        self.clearButton.clicked.connect(self.evt_clear_btn_click)
 
     def evt_opn_btn_click(self):
         try:
             global arr
             res = QtWidgets.QFileDialog.getOpenFileNames(self, 'Open File', '/', 'PDF File (*.pdf)')
             arr = res[0]
-            self.listWidget.clear()
 
             for file_name in res:
                 self.listWidget.addItems(file_name)
@@ -30,6 +32,11 @@ class DlgMain(QtWidgets.QMainWindow, design.Ui_MainWindow):
 
     def evt_sv_btn_click(self):
         global merger
+        arr = []
+        lst = self.listWidget
+        for x in range(lst.count()):
+            arr.append(lst.item(x).text())
+
         try:
             for pdf in arr:
                 merger.append(pdf)
@@ -43,6 +50,20 @@ class DlgMain(QtWidgets.QMainWindow, design.Ui_MainWindow):
         except Exception as exc:
             QtWidgets.QMessageBox.critical(self, 'Something wrong', 'Error')
             print(exc)
+
+    def evt_up_btn_click(self):
+        currentRow = self.listWidget.currentRow()
+        currentItem = self.listWidget.takeItem(currentRow)
+        self.listWidget.insertItem(currentRow - 1, currentItem)
+
+    def evt_down_btn_click(self):
+        currentRow = self.listWidget.currentRow()
+        currentItem = self.listWidget.takeItem(currentRow)
+        self.listWidget.insertItem(currentRow + 1, currentItem)
+
+    def evt_clear_btn_click(self):
+        arr = []
+        self.listWidget.clear()
 
 
 def main():
